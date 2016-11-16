e06e418a23d1d498b2f028ef67a2a74277dd7ea3
David Agbenyega <qafui@outlook.com>
2016-11-15 21:00:19 -0500

    Davids working version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,39 @@
  $(document).ready(function(){
-      $('.carousel').carousel();
-    });
+	 
+	 console.log('works');
+	 // Create Mood object that holds the properties of each mood
+	 // ==================================================
+	 var moodObj = {
+	 	"moods" : [
+	 		{
+	 			"mood" : "Happy",
+				"genre" : "Roulette"
+	 			"thumbnail" : "assets/images/happy.jpg",
+	 			
+	 		},
+	 		{
+	 			"mood" : "Cry Your Heart Out",
+				"genre": "Drama"
+	 			"thumbnail" : "assets/images/cry.jpg",
+	 		}
+	 	]
+	 }
+	 
+	 // Display mood on the home page
+	 // ==================================================
+	 function diplayMoods() {
+	 	
+		//Iterating through each mood
+		 $.each(moodObj.moods, function (key, value) {
+			 var thumb = $('img');
+			 thumb.addClass('moodThumb');
+			 thumb.attr(key.mood, value.mood);
+			 thumb.attr('src', 'value.thumbnail');
+			 $('#carousel').append(thumb);
+		 });
+	 }
+	 
+	 displayMoods();
+
+
+});
