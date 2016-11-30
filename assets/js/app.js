a49d488549fc379600f8f975f0a3342b22f852d7
David Agbenyega <qafui@outlook.com>
2016-11-29 19:08:08 -0500

    API calls successful

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,181 +1,73 @@
-$(document).ready(function (){
-	 
-	 //Global variables
-	 var moods = [];
-	 
-	 // Mood object function that holds the properties of each mood
-	 // =========================================================
-	 
-	 function Mood (name, genre, thumbURL, movieID) {
-		 this.moodName = name;							    //this.moodName sets the moodName within this function to the argument
-		 this.genre = genre;
-		 this.thumbnail = thumbURL;
-		 this.movieID = movieID;
+$( document ).ready(function () {
 
-		 		 
-		 //Method to display a mood's thumbnail
-		 this.displayMood = function() {
-			 var moodThumb = $('<img>');					//Create the a new image object
-			 moodThumb.attr('src',this.thumbnail);			//Set the src attribute of the image to the thumbnail URL
-			 moodThumb.attr('data-caption', this.genre);
-			 moodThumb.attr('movieID', this.movieID);
-			  //Set the data-caption attribute to the name of the object
-			 $('#moodRow').append(moodThumb);
-			moodThumb.wrap("<div class='col s4 mood' id='"+this.moodName+"'>"); 
-			 			//Append image to the page
-			 
-			 //Call moodClicked function when thumbnail is clicked
-			moodThumb.on('click', moodClicked);
-		 }
-	 }
-	 
-	 //=============Add Mood Objects function========================
-	 function addMood(moodName, genre, thumbnail, movieID) {
-		 moods[moods.length] = new Mood();					//Assign a new Mood object to the end of the moods array
-		 moods[moods.length-1].moodName = moodName;			//Set the properties of the new Mood object as follows
-		 moods[moods.length-1].genre = genre;
-		 moods[moods.length-1].thumbnail = thumbnail;
-		 moods[moods.length-1].movieID = movieID;
-		 moods[moods.length - 1].displayMood();				//Call the displayMood method to show the thumbnail image of the object
-	 }
-	 
-	 
-	 //==============Please Add and displaying moods here=============
-	 addMood('happy', 'comedy', 'assets/images/happy.jpg', '35');
-	 addMood('adventure', 'adventure', 'assets/images/adventurous.jpg', '12');
-	 addMood('Giddy', 'animated', 'assets/images/animated.jpg', '16');
-	 addMood('Dreamy', 'fantasy', 'assets/images/dream.jpg', '14');
-	 addMood('Kickass', 'action', 'assets/images/kickass.jpg', '28');
-	 addMood('Weird', 'scifi', 'assets/images/weird.jpg', '878');
-	 addMood('Smartypants', 'documentary', 'assets/images/smart.jpg', '99');
-	 addMood('Thriller', 'thriller', 'assets/images/thriller.jpg', '53');
-	 addMood('Nostalgic', 'classic', 'assets/images/nostalgic.jpg', '36');
-	 addMood('Horror', 'horror', 'assets/images/killer.png', '27');
-	 addMood('Dancey', 'musical', 'assets/images/dancey.jpg', '10402');
-	 addMood('Drama', 'drama', 'assets/images/drama.jpg', '18');
-	 
-	 //============= Victoria's Turn :-) ============
-	 function moodClicked() {
-		 console.log("clicked " + $(this).attr('data-caption'));  //Alert the name of the iamge
-		 
-		 //========= Call API using the genre clicked ================
-		 var genre = $(this).attr('data-caption');
-		 var api = "https://api.themoviedb.org/3/";
-		 var query = "discover/movie?with_genres=16&sort_by=vote_average.desc&vote_count.gte=10";
-		 var key = "&api_key=eb647f33ee48de066d1350aae8b20bc7";
-		 var url = api+query+key;
-		 console.log(url)
-		 $.ajax({url: url, method: 'GET'}).done(function(response) {
-		 	console.log(response)
-		 	var movies = response.results;
-		 	console.log(movies);
-
-		 	for (var i = 0; i < 3; i++){
-		 		if(movies[i].original_language == "en"){
-		 			moviesdbid = movies[i].id;
-
-		 			$.ajax({url: "https://api-public.guidebox.com/v1.43/US/KtvA54RB1hxQ6lToZTRuPEfyrFPIX0/search/movie/id/themoviedb/"+moviesdbid,
-		 					method: 'get'}).done(function(guidebox){
-		 						console.log(guidebox);
-		 					});
-		 			console.log(movies[i].id);
-		 		}
-		 	}
-		 });
-		
+    var moodClicked = function() {
+        $('.main-section').empty();
+        getMovies($(this).attr('data'));
     }
 
+    var getGenres = function () {
+        var query = "https://api.themoviedb.org/3/genre/movie/list?api_key=79cb508f9122735b0a8e6fbc7d17e61b&language=en-US";
+        $.ajax({
+            url: query,
+            method: 'GET'
+        }).done(function (response) {
+            //console.log(response.genres);
+            var genres = response.genres;
 
-    var Movie = function Movie (title, genre, actors,plot, poster, stream) {
-        this.movieTitle = title;
-        this.movieGenre = genre;
-        this.movieActors = actors
-        this.moviePlot = plot;
-        this.moviePoster = poster;
-        this.movieStream = stream;
+            for (var i=0; i < 14; i++) {
+                var genreItem = $('<a>');
+                genreItem.attr({
+                    'href': '#',
+                    'data': genres[i].id,
+                    'class': 'collection-item'
+                });
+                genreItem.html(genres[i].name);
+                genreItem.on('click', moodClicked);
+                $('#genres').append(genreItem);
+                genreItem.wrap('<li class="list-item"></li>');
 
-        this.displayPoster = function(){
+            }
 
-        };
+        });
     }
 
     var getMovies = function (genre) {
         this.genre = genre;
         var query = "https://api.themoviedb.org/3/discover/movie?api_key=79cb508f9122735b0a8e6fbc7d17e61b" +
-            "&language=en-US&sort_by=popularity.desc&certification_country=US" +
-            "&include_adult=false&include_video=true&page=1&with_genres=" + genre;
-        $.ajax({
-            url: query,
-            method: 'GET'
-        }).done(function(response) {
-            console.log(response);
-            return response;
-        });
-    };
+            "&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=truegit &page=1" +
+            "&with_genres=" + this.genre;
 
-    var getGenres = function () {
-        var query = "https://api.themoviedb.org/3/genre/movie/list?" +
-            "api_key=79cb508f9122735b0a8e6fbc7d17e61b&language=en-US";
 
         $.ajax({
             url: query,
             method: 'GET'
-        }).done(function(response) {
-            return response;
+        }).done( function(response) {
+            console.log(query);
+            var movies = response.results;
+            for (var i=0; i < 15; i++) {
+                var card = $('<div>');
+                card.attr({
+                    'class': 'card-medium col s3'
+                });
+                var movieCard = $('<img>');
+                movieCard.attr({
+                   'class': 'activator',
+                    'src' : 'https://image.tmdb.org/t/p/w300' + movies[i].poster_path
+                });
+                $('.main-section').append(movieCard);
+                movieCard.wrap("<div class='card-medium col s4'></div>");
+                movieCard.wrap("<div class='card-image waves-effect waves-block waves-light'></div>");
+
+            }
+
+
         });
     };
 
-    var runQuery = function(query) {
-        $.ajax().done();
-    }
 
-
+    getGenres();
 
 
 
 
 });
-
-
-
-//
-//
-// // test commit comment
-//
-// var movie1 = new Movie()
-//
-//  var movieAddOn = addOn
-//  addOn = $("#posterDiv")
-//  addOn.append("<p>" + movie.Title + "</p>");
-//  addOn.append("<p>" + movie.Genre + "</p>");
-//  addOn.append("<p>" + movie.Actors + "</p>");
-//  addOn.append("<p>" + movie.Plot + "</p>");
-//  addOn.append("<p>" + movie.Poster + "</p>")
-
- //poster_path - movie poster
-
- //var data-title = comedy
- //$("#dancey").first().data(comedy);
-
-
-
- //$('.img')on.('click', happenOnClick);
-
-
-
-
-//details 
-//<li class="details">
-//<button>details</button>
-//<ul class="trailer">Trailer:</ul>
-//<ul>Plot:</ul>
-//<ul>Stream:</ul>
-
-//
-// .trailer{
-// 	display:none;
-// }
-//
-// $('.details').on('click', 'button', function(){
-// 	$(this).closest('details').find('.trailer').slideDown();
-// })
