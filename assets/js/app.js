02ed861214fb1ce06b70647466bb88766235b93e
David Agbenyega <qafui@outlook.com>
2016-12-01 17:07:31 -0500

    testing homepage center carousel slide

--- assets/js/app.js
+++ assets/js/app.js
@@ -72,30 +72,30 @@ $( document ).ready(function () {
 
 
     getGenres();
-/*DEADPOOL*/
-var $poster = $('.card-container'),
-  $shine = $('.shine'),
-  w = $(window).width(),
-  h = $(window).height();
+    /*DEADPOOL*/
+    var $poster = $('.card-container'),
+        $shine = $('.shine'),
+        w = $(window).width(),
+        h = $(window).height();
 
-$(window).on('mousemove', function(e) {
-  var offsetX = 0.5 - e.pageX / w,
-    offsetY = 0.5 - e.pageY / h,
-    dy = e.pageY - h / 2,
-    dx = e.pageX - w / 2,
-    theta = Math.atan2(dy, dx),
-    angle = theta * 180 / Math.PI - 90,
-    offsetPoster = $poster.data('offset'),
-    transformPoster = 'translateY(' + -offsetX * offsetPoster + 'px) rotateX(' + (-offsetY * offsetPoster) + 'deg) rotateY(' + (offsetX * (offsetPoster * 2)) + 'deg)';
+    $('.row').on('mousemove', function (e) {
+        var offsetX = 0.5 - e.pageX / w,
+            offsetY = 0.5 - e.pageY / h,
+            dy = e.pageY - h / 2,
+            dx = e.pageX - w / 2,
+            theta = Math.atan2(dy, dx),
+            angle = theta * 180 / Math.PI - 90,
+            offsetPoster = $poster.data('offset'),
+            transformPoster = 'translateY(' + -offsetX * offsetPoster + 'px) rotateX(' + (-offsetY * offsetPoster) + 'deg) rotateY(' + (offsetX * (offsetPoster * 2)) + 'deg)';
 
-  if (angle < 0) {
-    angle = angle + 360;
-  }
-  $shine.css('background', 'linear-gradient(' + angle + 'deg, rgba(0,0,0,' + (e.pageY / h /5)  + ') 0%,rgba(0,0,0,.25) 80%)');
+        if (angle < 0) {
+            angle = angle + 360;
+        }
+        $shine.css('background', 'linear-gradient(' + angle + 'deg, rgba(0,0,0,' + (e.pageY / h / 5) + ') 0%,rgba(0,0,0,.25) 80%)');
 
-  $poster.css('transform', transformPoster);
-});
-/*DEADPOOL END*/
+        $poster.css('transform', transformPoster);
+    });
+    /*DEADPOOL END*/
 
 $(".collection-item").on("click", function() {
     //$(".mainContent").children().slideUp({duration: 1500});
