1d42f69b92cc16fbb55e3a1186a2c7d687582a23
David Agbenyega <qafui@outlook.com>
2016-12-01 21:20:06 -0500

    Inventive card functionality from MaterializeCSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,31 +5,17 @@ $( document ).ready(function () {
         getMovies($(this).attr('data'));
     }
 
-    var getGenres = function () {
-        var query = "https://api.themoviedb.org/3/genre/movie/list?api_key=79cb508f9122735b0a8e6fbc7d17e61b&language=en-US";
+    var getMovieDetail = function (movieID) {
+        var query = "https://api.themoviedb.org/3/movie/" + movieID +
+            "?api_key=79cb508f9122735b0a8e6fbc7d17e61b&language=en-US";
+
         $.ajax({
             url: query,
             method: 'GET'
-        }).done(function (response) {
-            //console.log(response.genres);
-            var genres = response.genres;
-
-            for (var i=0; i < 14; i++) {
-                var genreItem = $('<a>');
-                genreItem.attr({
-                    'href': '#',
-                    'data': genres[i].id,
-                    'class': 'collection-item'
-                });
-                genreItem.html(genres[i].name);
-                genreItem.on('click', moodClicked);
-                $('#genres').append(genreItem);
-                genreItem.wrap('<li class="list-item"></li>');
-
-            }
-
+        }).done( function(response) {
+            return response.imdb_id;
         });
-    }
+    };
 
     var getMovies = function (genre) {
         this.genre = genre;
@@ -44,34 +30,50 @@ $( document ).ready(function () {
         }).done( function(response) {
             // console.log(query);
             var movies = response.results;
-            console.log(movies);
-            console.log(movies);
             for (var i=0; i < 8; i++) {
+                console.log(movies[i]);
+                // 1. Create image element
+                var movieImg = $('<img>');
+                movieImg.attr({
+                    'class': 'activator',
+                    'src': 'https://image.tmdb.org/t/p/w185' + movies[i].poster_path
+                });
 
-                console.log(movies[i].poster_path);
-                    var card = $('<div>');
-                    card.attr({
-                        'class': 'card-medium col s3'
-                    });
-                    var movieCard = $('<img>');
-                    if(movies[i].poster_path != null) {
-                        movieCard.attr({
-                            'class': 'activator',
-                            'src' : 'https://image.tmdb.org/t/p/w185' + movies[i].poster_path
-                        });
-                    }
+                // 2. Create card content
+                var cardContent = $('<span>');
+                cardContent.attr({
+                    'class' : 'card-title activator grey-text text-darken-4',
+                    'id' : movies[i].id
+                });
+                cardContent.wrapInner("<i class='material-icons right'>more_vert</i>");
+                cardContent.html(movies[i].original_title);
 
-                    $('.mainContent').append(movieCard);
-                    movieCard.wrap("<div class='card-medium col s3'></div>");
-                    movieCard.wrap("<div class='card-image waves-effect waves-block waves-light'></div>");
-                }
+                // 3. Create card reveal section
+
+
+                // 4. Append image to mainContent
+                $('.mainContent').append(movieImg);
+
+                // 5. Wrap image with card-medium class
+                movieImg.wrap("<div class='card-medium col s3'></div>");
+
+                // 6. Wrap image with again with waves class
+                movieImg.wrap("<div class='card-image waves-effect waves-block waves-light'></div>");
+
+                //$("'#"+movies[i].id+"'").add(cardContent);
+                movieImg.parent().after(cardContent);
+                cardContent.wrap("<div class='card-content'></div>");
+            }
 
 
         });
     };
 
 
-    getGenres();
+
+    getMovieDetail(259316);
+
+    //getGenres();
     /*DEADPOOL*/
     var $poster = $('.card-container'),
         $shine = $('.shine'),
@@ -98,9 +100,9 @@ $( document ).ready(function () {
     /*DEADPOOL END*/
 
 $(".collection-item").on("click", function() {
-    //$(".mainContent").children().slideUp({duration: 1500});
-    $(".mainContent").empty();
-    //Materialize.showStaggeredList('#staggered-test')
+    $(".mainContent").children().slideUp({duration: 1500});
+    //$(".mainContent").empty();
+    //Materialize.showStaggerecdList('#staggered-test')
     getMovies($(this).attr('data'));
 });
 
